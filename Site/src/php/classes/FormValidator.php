85414f638682a3351034a8ed448d93b2dffa9495
Yann Merk <yannmerk@gmail.com>
2017-03-07 16:31:37 +0100

    Worked on login, the redirect should work

---
+++ Site/src/php/classes/FormValidator.php
@@ -0,0 +1,15 @@
+<?php
+
+/**ETML
+ *\author  merkya
+ *\date    07.03.2017
+ *\summary
+ */
+class FormValidator
+{
+    public static function checkLogin(string $email, string $password)
+    {
+        //TODO
+        return true;
+    }
+}
--- Site/src/php/login.php
+++ Site/src/php/login.php
@@ -1,26 +1,47 @@
 <?php
 $pageId = 5;
 require_once("before.php");
+
+//check if there's an attempt to login
+if (isset($_POST["email"]) && isset($_POST["pswd"])) {
+    //check if the log is successfull or no
+    if (FormValidator::checkLogin($_POST["email"], $_POST["pswd"])) {
+
+        $_SESSION["isConnected"] = true;
+        $_SESSION["mustShowPopup"] = true;
+
+        // redirect to the previous page, or the index page if none is inputed
+        if (isset($_GET["previousPageID"])) {
+            header("location: /" . GlobalValue::PAGES_ARRAY[$_GET["previousPageID"]][1]);
+        } else {
+            header("location: /");
+        }
+    } else {
+        //if not, display an error
+
+        //TODO
+    }
+}
 ?>
     <div class="container">
         <div class="row">
             <div class="col-sm-3"></div>
             <div class="col-sm-6">
-                <form method="post" action="#">
+                <form method="post">
                     <div class="input-group">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                        <input id="email" type="text" class="form-control" name="email" placeholder="Email">
+                        <input id="email" type="text" class="form-control" name="email" placeholder="Email" required>
                     </div>
                     <div class="input-group">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                        <input id="password" type="password" class="form-control" name="password"
-                               placeholder="Mot de passe">
+                        <input id="pswd" type="password" class="form-control" name="pswd"
+                               placeholder="Mot de passe" required>
                     </div>
+                    <button type="submit" class="btn btn-default">Login</button>
                 </form>
             </div>
             <div class="col-sm-3"></div>
         </div>
-
     </div>
 <?php
 require_once("after.php");
