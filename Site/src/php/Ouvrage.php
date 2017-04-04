e72b222ca56691e2d8c1c456d74cceacbc7d61dd
Yann Merk <yannmerk@gmail.com>
2017-04-04 16:33:41 +0200

    Commencé à travailler sur le SQL de la page Ouvrages

--- Site/src/php/Ouvrage.php
+++ Site/src/php/Ouvrage.php
@@ -4,7 +4,18 @@
 //Récupères tous les livres
 
 //Si il y suffisament de livre
-//TODO Boucler sur toutes les lignes de livres
+    $books = DBCom::getAllBooks();
+    if (count($books) > 0) {
+
+        //TODO Boucler sur toutes les lignes de livres
+        foreach ($books as $book) {
+            echo '<div class="bookElement col-sm-2">';
+                echo'   <p>'.$book['booTitle'].'</p>';
+            echo'</div>';
+        }
+    } else {
+        Misc::writeMessage(2, "Il n'y a aucun livre pour le moment T-T");
+    }
 ?>
 
     <div class="row"></div>
